830cb73013ba69ca316e2a64789f716f8edd1990
Anna Lee <twenty2ight@gmail.com>
2017-11-25 15:14:17 +0900

    feat: Update nb_author_id.py
    Created a prediction model

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -25,8 +25,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
-### your code goes here ###
+### my code goes here ###
 
+# Import the relevant module:
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# Create an instance of a classifier
+clf = GaussianNB()
+
+# fit that instance, using the training data
+timestamp = time() # Current time
+clf.fit(features_train, labels_train)
+print "Time taken for training:", round(time()-timestamp, 3), "s"
+
+# Now I have a fitted model
+# Make a prediction using the test data
+timestamp = time() # Current time
+pred = clf.predict(features_test)
+print "Time taken for prediction:", round(time()-timestamp, 3), "s"
+
+# Estimate the accuracy
+print "Accuracy:", accuracy_score(pred, labels_test)
 
 #########################################################
 
