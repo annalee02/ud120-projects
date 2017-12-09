3ab12f98ef9b9bd4177ea39d488db2cf635bc05f
Anna Lee <twenty2ight@gmail.com>
2017-12-09 23:27:25 +0900

    feat: Update parse_out_email_text.py

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -8,12 +8,12 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
+
         """
 
 
@@ -33,14 +33,14 @@ def parseOutText(f):
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
+        stemmer = SnowballStemmer("english")
+        text_string = text_string.split()
+        for text in text_string:
+            words = words + stemmer.stem(text) + " "
 
     return words
 
-    
+
 
 def main():
     ff = open("../text_learning/test_email.txt", "r")
@@ -51,4 +51,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
